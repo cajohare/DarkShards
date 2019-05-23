316f530b82b8e4ee4f914555775c4875e5960190
cajohare <ciaran.aj.ohare@gmail.com>
2019-05-23 22:29:31 +0200

    speed distribution plots

--- code/PlotFuncs.py
+++ code/PlotFuncs.py
@@ -1,9 +1,11 @@
 from numpy import *
 from numpy.random import *
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 #from mpl_toolkits.basemap import Basemap
 import matplotlib.gridspec as gridspec
+from matplotlib.colors import ListedColormap
 from matplotlib import colors
 import pandas
 from scipy.spatial import Delaunay
@@ -21,6 +23,44 @@ from astropy import units
 from skimage import measure
 
 Sun = array([8.122,0.0,0.005])
+
+
+def reverse_colourmap(cmap, name = 'my_cmap_r'):
+    """
+    In: 
+    cmap, name 
+    Out:
+    my_cmap_r
+
+    Explanation:
+    t[0] goes from 0 to 1
+    row i:   x  y0  y1 -> t[0] t[1] t[2]
+                   /
+                  /
+    row i+1: x  y0  y1 -> t[n] t[1] t[2]
+
+    so the inverse should do the same:
+    row i+1: x  y1  y0 -> 1-t[0] t[2] t[1]
+                   /
+                  /
+    row i:   x  y1  y0 -> 1-t[n] t[2] t[1]
+    """        
+    reverse = []
+    k = []   
+
+    for key in cmap._segmentdata:    
+        k.append(key)
+        channel = cmap._segmentdata[key]
+        data = []
+
+        for t in channel:                    
+            data.append((1-t[0],t[2],t[1]))            
+        reverse.append(sorted(data))    
+
+    LinearL = dict(zip(k,reverse))
+    my_cmap_r = mpl.colors.LinearSegmentedColormap(name, LinearL) 
+    return my_cmap_r
+
 
 
 def MySquarePlot(xlab='',ylab='',\
